7d2b41d2307b80ff32a31f43f4cea7fd0ce1b01b
Jay Drage <drag1741@gmail.com>
2018-12-11 07:30:43 -0800

    reworked the order of main.c and added fprintf to stderr

--- src/main.c
+++ src/main.c
@@ -1,50 +1,44 @@
-//main.c
-//dungeon exploration game
+/*  File: main.c
+    Purpose: dungeon exploration game
+    Author: Jay Drage
+*/
 
 #include <ncurses.h>
 #include <stdlib.h>
+
 #include "dungeon.h"
 #include "character.h"
 #include "utility.h"
 
 int main(int argc, char *argv[]){
-/*
-    if(argc < 4){
-        printf("[usage] %s height width density\n", argv[0]);
-        exit(-1);
-    }
-*/  
-    //initialize game struct
+    /*********initialize game struct*********/
     init_game();
-
-    //initialize Dungeon struct
+    /********initialize Dungeon struct**********/
     struct Dungeon *dungeon = init_dungeon(10);
-    if( dungeon == NULL)//failed to initialize
-        exit(-1);
-    //initialize Player
+    if(dungeon == NULL){//failed to initialize
+       fprintf(stderr,"[main] dungeon failed to initialize.\n");
+       exit(-1);
+    }
+    /********initialize Player**********/
     struct Character *player=init_character('@',dungeon->floors[0]->entrances[0][0],
                                                 dungeon->floors[0]->entrances[0][1]);
-    if( player == NULL)//failed to initialize
-        exit(-2);
-
-    //print out current state to screen
-    dungeon->print_current_floor(dungeon);
-    player->print_character(player);
-
-    //main game loop
+    if(player == NULL){//failed to initialize
+       fprintf(stderr,"[main] player failed to initialize.\n");
+       exit(-2);
+    }
+    /********main game loop**********/
     int input = 0;
-    while((input = wgetch(dungeon_win)) != 'q'){
-       //clear();
-       //input handling
-       input_handle(dungeon, player, input);
+    while(input != 'q'){
        //update screen
        dungeon->print_current_floor(dungeon);
        player->print_character(player);
        wrefresh(dungeon_win);
        wrefresh(info_win);
+       //input handling
+       input = wgetch(dungeon_win);
+       input_handle(dungeon, player, input);
     }
-
-    //close window and program
+    /**********close window and program************/
     dungeon->delete_dungeon(dungeon);
     endwin();
     return(0);
